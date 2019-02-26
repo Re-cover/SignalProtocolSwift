dd89dbfc78ec32a229b8da91e644d53e674929d5
TerryHu <thusterry@gmail.com>
2019-02-26 14:10:45 +0800

    Initial Commit

---
+++ SignalProtocolSwift/SignalProtocolSwift.h
@@ -0,0 +1,19 @@
+//
+//  SignalProtocolSwift.h
+//  SignalProtocolSwift
+//
+//  Created by Terry on 2019/2/26.
+//  Copyright © 2019 SKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SignalProtocolSwift.
+FOUNDATION_EXPORT double SignalProtocolSwiftVersionNumber;
+
+//! Project version string for SignalProtocolSwift.
+FOUNDATION_EXPORT const unsigned char SignalProtocolSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalProtocolSwift/PublicHeader.h>
+
+
